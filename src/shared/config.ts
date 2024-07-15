263821621f81c36aa8b20bbcb2c17ba257333631
zhaohx <zhaohx0409@163.com>
2024-07-15 12:44:45 +0800

    v1.1.3

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,5 +1,5 @@
 export const sdk_name = 'motojs_sdk'
-export const sdk_version = '1.1.2'
+export const sdk_version = '1.1.3'
 
 export const header_sdk_name = 'Sdk-Name'
 export const header_sdk_version = 'Sdk-Version'
